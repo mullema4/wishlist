987a0683cce23c7cd032d49b33945a5f45e61c78
muller <muller@cngroup.dk>
2019-11-06 18:19:12 +0100

    Added restriction to limit exposure of repository methods to REST API

--- src/main/java/dk/cngroup/fetch/repository/ClientRepository.java
+++ src/main/java/dk/cngroup/fetch/repository/ClientRepository.java
@@ -3,11 +3,14 @@ package dk.cngroup.fetch.repository;
 import dk.cngroup.fetch.entity.Client;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 public interface ClientRepository extends CrudRepository<Client, Long> {
 
+	@RestResource(exported = false)
 	Client getByUsername(String username);
 
+	@RestResource(exported = false)
 	@EntityGraph(attributePaths = {"wishes"})
 	Client findByUsername(String username);
 
