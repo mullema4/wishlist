446ddb9b51b4ea4eec158ad5fd3f68a9d019d87c
muller <muller@cngroup.dk>
2019-11-08 01:48:23 +0100

    Custom controller; fixed serialization cyclic reference

--- src/main/java/dk/cngroup/fetch/repository/WishlistRepository.java
+++ src/main/java/dk/cngroup/fetch/repository/WishlistRepository.java
@@ -2,7 +2,6 @@ package dk.cngroup.fetch.repository;
 
 import dk.cngroup.fetch.entity.Wishlist;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 
 public interface WishlistRepository extends JpaRepository<Wishlist, Long> {
 }
