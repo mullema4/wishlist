e4852627a851296c45a6cc7465bc524942c3016b
muller <muller@cngroup.dk>
2020-06-13 23:59:13 +0200

    javax.validation example

--- src/main/java/dk/cngroup/fetch/entity/Product.java
+++ src/main/java/dk/cngroup/fetch/entity/Product.java
@@ -3,10 +3,12 @@ package dk.cngroup.fetch.entity;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.data.rest.core.annotation.Description;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Getter
@@ -16,6 +18,8 @@ public class Product {
 	@Id
 	Long id;
 
+	@NotNull
+	@Length(min = 3, max = 30)
 	@Description("Serial number of the item")
 	String code;
 }
