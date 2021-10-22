b184215d2c399337617cc0636ce2e8be823ef333
muller <muller@cngroup.dk>
2021-10-22 13:45:02 +0200

    JPA auditing concept

--- src/main/java/dk/cngroup/fetch/DemoApplication.java
+++ src/main/java/dk/cngroup/fetch/DemoApplication.java
@@ -2,8 +2,10 @@ package dk.cngroup.fetch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
---
+++ src/main/java/dk/cngroup/fetch/entity/AuditableEntity.java
@@ -0,0 +1,27 @@
+package dk.cngroup.fetch.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class AuditableEntity {
+
+    @CreatedDate
+    private LocalDateTime created;
+    @LastModifiedDate
+    private LocalDateTime updated;
+    @CreatedBy
+    private LocalDateTime createdBy;
+
+}
