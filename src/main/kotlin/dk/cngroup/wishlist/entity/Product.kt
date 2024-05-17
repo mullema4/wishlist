b9ce1dc66bee66964a0c7b08ef419d810a220ac1
jandam15 <97833075+jandam15@users.noreply.github.com>
2024-05-17 13:06:41 +0200

    Add optional description field 1-255 chars long to Product entity

--- src/main/kotlin/dk/cngroup/wishlist/entity/Product.kt
+++ src/main/kotlin/dk/cngroup/wishlist/entity/Product.kt
@@ -4,12 +4,15 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.rest.core.annotation.Description
 import jakarta.persistence.Entity
 import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.Size
 
 @Entity
 class Product(
     @Description("Unique name of the item")
     @field:NotNull
-    var code: String
+    var code: String,
+    @field:Size(min = 1, max = 255)
+    var description: String? = null
 ) : AuditableEntity()
 
 interface ProductRepository : JpaRepository<Product?, Long?>
